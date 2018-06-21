d55b481afaa2ba85718cb21a9b2e0cb40eab54d8
小枫 <1317847793@qq.com>
2018-06-21 21:16:04 +0800

    界面

---
+++ app/src/main/java/com/ldxiaofeng/criminalintent/Crime.java
@@ -0,0 +1,57 @@
+package com.ldxiaofeng.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by 13178 on 2018-4-11.
+ */
+
+public class Crime {
+
+    private UUID mId;
+
+    private String mTitle;
+
+    private Date mDate;
+
+    private boolean mSolved;
+
+    public Crime(){
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public void setId(UUID id) {
+        mId = id;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
+
